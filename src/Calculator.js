588945bca4dfb188fbfe083215271a11697ccd10
DESKTOP-N4G7Q1I\todag <vo.thi.huyen.trang@geonet.co.jp>
2021-06-20 09:46:17 +0700

    +update useState

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import "./Calculator.css";
 import Keyboard from "./Keyboard";
+import { BiHistory } from "react-icons/bi";
 const Calculator = () => {
+  const [result, setResult] = useState(0);
+  const [fomula, setFomula] = useState("");
   return (
     <div className="calculator">
       <div className="history">
         <div>
-          <i class="fa fa-history historyicon"></i>
+          <BiHistory color={"#ffff"} />
         </div>
-        <div className="historyvalue">5000</div>
+        <div className="historyvalue">{fomula}</div>
       </div>
       <div className="result">
-        <div>0</div>
+        <div>{result}</div>
       </div>
       <div className="key-board">
-        <Keyboard />
+        <Keyboard
+          onChange={(newFomula) => {
+            setFomula(newFomula);
+          }}
+          onCompute={(newResult) => {
+            setResult(newResult);
+          }}
+        />
       </div>
+      <button
+        onClick={() => {
+          setResult(1);
+        }}
+      >
+        change
+      </button>
       <div className="footer"></div>
     </div>
   );
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,44 +1,199 @@
 import "./Keyboard.css";
-const Keyboard = () => {
+import { BsArrowLeft } from "react-icons/bs";
+import { useState } from "react";
+const Keyboard = (props) => {
+  const [fomula, setFomula] = useState("");
   return (
     <div>
       <div className="row">
-        <div className="board">
-          0
-        </div>
+        <div className="board">0</div>
       </div>
       <div className="keyboard">
         <div className="row">
-          <div className="key">AC</div>
-          <div className="key">v</div>
-          <div className="key2">%</div>
-          <div className="key2">+-</div>
-          <div className="key ">/</div>
+          <div
+            className="key"
+            onClick={() => {
+              setFomula("");
+              props.onChange("");
+            }}
+          >
+            AC
+          </div>
+          <div className="key">
+            <BsArrowLeft />
+          </div>
+          <div
+            className="key2"
+            onClick={() => {
+              console.log("%");
+            }}
+          >
+            %
+          </div>
+          <div
+            className="key2"
+            onClick={() => {
+              console.log("+-");
+            }}
+          >
+            +-
+          </div>
+          <div
+            className="key "
+            onClick={() => {
+              console.log("/");
+            }}
+          >
+            /
+          </div>
         </div>
         <div className="row">
-          <div className="key background-color1">7</div>
-          <div className="key background-color1">8</div>
-          <div className="key background-color1">9</div>
-          <div className="key">x</div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              setFomula(fomula + "7");
+              props.onChange(fomula + "7");
+            }}
+          >
+            7
+          </div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              setFomula(fomula + "8");
+              props.onChange(fomula + "8");
+            }}
+          >
+            8
+          </div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              setFomula(fomula + "9");
+              props.onChange(fomula + "9");
+            }}
+          >
+            9
+          </div>
+          <div
+            className="key"
+            onClick={() => {
+              setFomula(fomula + "x");
+              props.onChange(fomula + "x");
+            }}
+          >
+            x
+          </div>
         </div>
         <div className="row">
-          <div className="key background-color1">4</div>
-          <div className="key background-color1">5</div>
-          <div className="key background-color1">6</div>
-          <div className="key">-</div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              console.log("4");
+            }}
+          >
+            4
+          </div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              console.log("5");
+            }}
+          >
+            5
+          </div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              console.log("6");
+            }}
+          >
+            6
+          </div>
+          <div
+            className="key"
+            onClick={() => {
+              console.log("-");
+            }}
+          >
+            -
+          </div>
         </div>
         <div className="row">
-          <div className="key background-color1">1</div>
-          <div className="key background-color1">2</div>
-          <div className="key background-color1">3</div>
-          <div className="key">+</div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              console.log("1");
+            }}
+          >
+            1
+          </div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              console.log("2");
+            }}
+          >
+            2
+          </div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              console.log("3");
+            }}
+          >
+            3
+          </div>
+          <div
+            className="key"
+            onClick={() => {
+              console.log("+");
+            }}
+          >
+            +
+          </div>
         </div>
         <div className="row">
-          <div className="key background-color1">0</div>
-          <div className="key background-color1">.</div>
-          <div className="key2 background-color1">(</div>
-          <div className="key2 background-color1">)</div>
-          <div className="key background-color2">=</div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              console.log("0");
+            }}
+          >
+            0
+          </div>
+          <div
+            className="key background-color1"
+            onClick={() => {
+              console.log(".");
+            }}
+          >
+            .
+          </div>
+          <div
+            className="key2 background-color1"
+            onClick={() => {
+              console.log("(");
+            }}
+          >
+            (
+          </div>
+          <div
+            className="key2 background-color1"
+            onClick={() => {
+              console.log(")");
+            }}
+          >
+            )
+          </div>
+          <div
+            className="key background-color2"
+            onClick={() => {
+              props.onCompute(56)
+            }}
+          >
+            =
+          </div>
         </div>
       </div>
     </div>
